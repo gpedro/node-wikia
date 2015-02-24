4258a76be64fb11362fb6530d27c2d3e7cf3e092
Gabriel Pedro <gpedro831@gmail.com>
2015-02-24 07:12:04 -0400

    Adding Test Folder

---
+++ test/index.js
@@ -0,0 +1,10 @@
+/*global describe, it */
+'use strict';
+var assert = require('assert');
+var Wikia = require('../');
+
+describe('node-wikia node module', function () {
+  it('to do', function () {
+    assert(false, false);
+  });
+});
