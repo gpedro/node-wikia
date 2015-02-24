47115813ca4a1d2b47e9efbac8aaebae7b687764
Gabriel Pedro <gpedro831@gmail.com>
2015-02-24 08:49:01 -0400

    Simple TEst

--- test/index.js
+++ test/index.js
@@ -1,10 +1,17 @@
 /*global describe, it */
 'use strict';
 var assert = require('assert');
+var is = require('is');
 var Wikia = require('../');
+var tibia = new Wikia('tibia');
 
-describe('node-wikia node module', function () {
-  it('to do', function () {
-    assert(false, false);
+describe('node-wikia requests', function () {
+  it('getNavigation()', function () {
+    tibia.getNavigation().then(function(data) {
+      assert(is.object(data), true);
+    })
+    .fail(function(err) {
+      throw err;
+    });
   });
 });
